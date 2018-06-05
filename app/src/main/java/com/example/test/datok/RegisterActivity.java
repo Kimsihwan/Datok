08d758f95bd6002558fcc3228d8f9696aab19a62
Kimsihwan <namid882@gmail.com>
2018-06-06 01:28:56 +0900

    사용자 등록

---
+++ app/src/main/java/com/example/test/datok/RegisterActivity.java
@@ -0,0 +1,77 @@
+package com.example.test.datok;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+
+    private TextInputLayout mDisplayname;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+    private Button mCreateBtn;
+
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mDisplayname = findViewById(R.id.reg_display_name);
+        mEmail = findViewById(R.id.reg_email);
+        mPassword = findViewById(R.id.reg_password);
+        mCreateBtn = findViewById(R.id.reg_create_btn);
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String display_name = mDisplayname.getEditText().getText().toString();
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
+
+                register_user(display_name, email, password);
+
+            }
+        });
+
+
+    }
+
+    private void register_user(String display_name, String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "회원가입 성공");
+                            Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(mainIntent);
+                            finish();
+                        } else {
+                            Log.d(TAG, "회원가입 실패: ", task.getException());
+                            Toast.makeText(RegisterActivity.this, "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
