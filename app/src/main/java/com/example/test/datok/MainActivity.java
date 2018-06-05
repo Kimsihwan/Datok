1130031e4dff5e14c4e3d489e0ab9b27cb3579c6
Kimsihwan <namid882@gmail.com>
2018-06-06 01:46:39 +0900

    로그아웃 및 app bar 만들기

--- app/src/main/java/com/example/test/datok/MainActivity.java
+++ app/src/main/java/com/example/test/datok/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.test.datok;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -10,6 +14,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("DaTok");
+
+
     }
 
     @Override
@@ -28,11 +39,36 @@ public class MainActivity extends AppCompatActivity {
 
         if(currentUser == null) {
 
-            Intent startIntetnt = new Intent(MainActivity.this, StartActivity.class);
-            startActivity(startIntetnt);
-            finish();
+            sendToStart();
+
         }
 
     }
 
+    private void sendToStart() {
+        Intent statIntetnt = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(statIntetnt);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.main_logout_btn) {
+            FirebaseAuth.getInstance().signOut(); // 로그아웃
+            sendToStart();
+        }
+
+        return true;
+    }
 }
