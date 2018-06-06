a0a246d70c35f6a15b22b6f21da5b28d153c6008
Kimsihwan <namid882@gmail.com>
2018-06-06 15:31:22 +0900

    파이어베이스에 프로필사진 업로드

--- app/src/main/java/com/example/test/datok/SettingsActivity.java
+++ app/src/main/java/com/example/test/datok/SettingsActivity.java
@@ -1,13 +1,18 @@
 package com.example.test.datok;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -15,10 +20,19 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private static final String TAG = "SettingsActivity";
 
     private DatabaseReference mUserDatabase;
     private FirebaseUser mCurrentUser;
@@ -31,6 +45,12 @@ public class SettingsActivity extends AppCompatActivity {
     private Button mStatusBtn;
     private Button mImageBtn;
 
+    private static final int GALLERY_PICK = 1;
+
+    //Storage Firebase
+
+    private StorageReference mImageStorage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,9 +59,11 @@ public class SettingsActivity extends AppCompatActivity {
         mDisplayImage = findViewById(R.id.settings_image);
         mName = findViewById(R.id.settings_display_name);
         mStatus = findViewById(R.id.settings_status);
+
         mStatusBtn = findViewById(R.id.setting_satus_btn);
         mImageBtn = findViewById(R.id.setting_image_btn);
 
+        mImageStorage = FirebaseStorage.getInstance().getReference();
 
         mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -72,7 +94,6 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String status_value = mStatus.getText().toString();
 
-
                 Intent status_intent = new Intent(SettingsActivity.this, StatusActivity.class);
                 status_intent.putExtra("status_value", status_value);
                 startActivity(status_intent);
@@ -80,5 +101,96 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        mImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(galleryIntent, "SELECT IMAGE"), GALLERY_PICK);
+
+
+                // start picker to get image for cropping and then use the image in cropping activity
+                // 자르기를위한 이미지를 얻기 위해 선택 도구를 시작한 다음 자르기 활동에서 이미지를 사용합니다.
+                /*
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(SettingsActivity.this);
+                        */
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_PICK && resultCode == RESULT_OK) {
+
+            Uri imageUri = data.getData();
+
+            // start cropping activity for pre-acquired image saved on the device
+            // 장치에 저장된 사전 획득 이미지에 대한 자르기 활동 시작
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+
+            //Toast.makeText(SettingsActivity.this, imageUri, Toast.LENGTH_SHORT).show();
+
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if (resultCode == RESULT_OK) {
+
+                Uri resultUri = result.getUri();
+
+                StorageReference filepath = mImageStorage.child("profile_images").child(random() + ".jpg");
+
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                        if(task.isSuccessful()) {
+
+                            Log.d(TAG, "프로필 사진 업로드 성공");
+
+                            Toast.makeText(SettingsActivity.this, "프로필 사진 업로드 성공", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Log.d(TAG, "프로필 사진 업로드 실패", task.getException());
+
+                            Toast.makeText(SettingsActivity.this, "업로드 실패", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+
+                Exception error = result.getError();
+
+            }
+        }
+
+
+    }
+
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(10);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++) {
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
     }
 }
