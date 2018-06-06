8ce8dfb2c13ef963a4ee8175eb6a22c842e33cdc
Kimsihwan <namid882@gmail.com>
2018-06-06 19:53:31 +0900

    친구요청 파이어베이스 설정

--- app/src/main/java/com/example/test/datok/ProfileActivity.java
+++ app/src/main/java/com/example/test/datok/ProfileActivity.java
@@ -1,25 +1,125 @@
 package com.example.test.datok;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private TextView mDisplayID;
+    private ImageView mProfileImage;
+    private TextView mProfileName, mProfileStatus, mProfileFriendsCount;
+    private Button mProfileSendReqBtn;
+
+    private DatabaseReference mUserDatabase;
+
+    private ProgressDialog mProgressDialog;
+
+    private DatabaseReference mFriendReqDatabase;
+
+    private FirebaseUser mCurrent_user;
+
+    private String mCurrent_state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        String user_id =  getIntent().getStringExtra("user_id");
+        final String user_id =  getIntent().getStringExtra("user_id");
 
-        mDisplayID = findViewById(R.id.profile_displayName);
-        mDisplayID.setText(user_id);
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+        mFriendReqDatabase = FirebaseDatabase.getInstance().getReference().child("Friend_req");
+        mCurrent_user = FirebaseAuth.getInstance().getCurrentUser();
 
-        
+        mProfileImage = findViewById(R.id.profile_image);
+        mProfileName = findViewById(R.id.profile_displayName);
+        mProfileStatus = findViewById(R.id.profile_status);
+        mProfileFriendsCount = findViewById(R.id.profile_totalFriends);
+        mProfileSendReqBtn = findViewById(R.id.profile_send_req_btn);
 
+
+
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setTitle("유저 불러오는 중");
+        mProgressDialog.setMessage("잠시만 기다려 주세요.");
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        mProgressDialog.show();
+
+
+        mCurrent_state = "친구없음";
+
+
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String display_name = dataSnapshot.child("name").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+
+                mProfileName.setText(display_name);
+                mProfileStatus.setText(status);
+
+                Picasso.get().load(image).placeholder(R.drawable.ic_action_name).into(mProfileImage);
+
+                mProgressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mProfileSendReqBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mCurrent_state.equals("친구없음")) {
+
+                    mFriendReqDatabase.child(mCurrent_user.getUid()).child(user_id).child("request_type").setValue("sent").addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful()) {
+
+                                mFriendReqDatabase.child(user_id).child(mCurrent_user.getUid()).child("request_type")
+                                        .setValue("received").addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+
+                                        Toast.makeText(ProfileActivity.this, "친구요청 성공", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+
+                            } else {
+
+                                Toast.makeText(ProfileActivity.this, "친구요청 실패", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        }
+                    });
+                }
+            }
+        });
 
 
     }
